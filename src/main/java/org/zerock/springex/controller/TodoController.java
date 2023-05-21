a9d5e96cdf92e6730643707eb9c4ab5d256dd341
sonbuwon <bwgh0268@naver.com>
2023-05-21 22:11:10 +0900

    하나의 클래스에서 여러 컨트롤러들을 매핑

---
+++ src/main/java/org/zerock/springex/controller/TodoController.java
@@ -0,0 +1,28 @@
+package org.zerock.springex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todo")
+@Log4j2
+public class TodoController {
+
+    @RequestMapping("/list")
+    public void list() {
+        log.info("todo list...........");
+    }
+
+    @GetMapping("/register")
+    public void registerGET() {
+        log.info("GET todo register............");
+    }
+
+    @PostMapping("/register")
+    public void registerPOST() {
+        log.info("POST todo register................");
+    }
+}
