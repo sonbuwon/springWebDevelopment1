ca2bcdf736df7e6c11d0bbf6e62d29ed7a9dcd49
sonbuwon <bwgh0268@naver.com>
2023-05-22 21:47:38 +0900

    스프링 너란 자식 세팅 할게 너무 많아............

--- src/main/java/org/zerock/springex/controller/SampleController.java
+++ src/main/java/org/zerock/springex/controller/SampleController.java
@@ -3,6 +3,9 @@ package org.zerock.springex.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
 
 @Controller
 @Log4j2
@@ -11,5 +14,28 @@ public class SampleController {
     @GetMapping("/hello")
     public void hello() {
         log.info("hello.................");
+    }
+
+    // 단순 파라미터의 자동 수집
+    @GetMapping("/ex1")
+    public void ex1(String name, int age) {
+        log.info("ex1...........");
+        log.info("name: " + name);
+        log.info("age: " + age);
+    }
+
+    @GetMapping("/ex2")
+    public void ex2(@RequestParam(name = "name", defaultValue = "sonbuwon") String name,
+                    @RequestParam(name = "age", defaultValue = "29") int age) {
+        log.info("ex2................");
+        log.info("name: " + name);
+        log.info("age: " + age);
+    }
+
+    // Formatter를 이용한 파라미터의 커스텀 처리
+    @GetMapping("/ex3")
+    public void ex3(LocalDate dueDate) {
+        log.info("ex3...................");
+        log.info("dueDate: " + dueDate);
     }
 }
